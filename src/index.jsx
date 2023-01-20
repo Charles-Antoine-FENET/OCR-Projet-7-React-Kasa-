45675263acacf0d0ceb1fd7e82dcbed75dd1245c
Charles-Antoine FENET <108330821+Charles-Antoine-FENET@users.noreply.github.com>
2023-01-20 10:38:15 +0100

    Update to the latest versions of dependencies and refactoring with new syntax

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Accommodation from './pages/Accomodation'
 import About from './pages/About'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
     <Router>
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/accommodation">
-          <Accommodation />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/accommodation" element={<Accommodation />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,16 +5,17 @@ import HomeBannerIllustration from '../../assets/HomeBanner.jpg'
 import Logo from '../../assets/logo_primary.png'
 import colors from '../../utils/style/colors'
 
+
 const CardsContainer = styled.div`
   display: flex;
-  flex-wrap:wrap;
+  flex-wrap: wrap;
   row-gap: 24px;
   align-items: center;
   justify-content: space-around;
   background-color: ${colors.backgroundLight};
   border-radius: 25px;
   padding: 25px;
-  margin : 50px 0px
+  margin: 50px 0px;
 `
 
 function Home() {
