f3ff64a2f1581106ef4a1ab807096971d76c07ad
Charles-Antoine FENET <108330821+Charles-Antoine-FENET@users.noreply.github.com>
2023-02-20 16:08:59 +0100

    Refacto Rating

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,17 +1,27 @@
 import fullStar from '../../assets/img/fullStar.png'
 import star from '../../assets/img/star.png'
 import styles from './Rating.module.scss'
+import { useState, useEffect } from 'react'
+
 
 function Rating({ rating }) {
-  const rate = []
-  for (let i = 0; i < 5; i++) {
-    if (i < rating) {
-      rate.push(<img className={styles.starsRating} src={fullStar} alt="Etoile pleine" key={i} />)
-    } else {
-      rate.push(<img className={styles.starsRating} src={star} alt="Etoile vide" key={i} />)
+  const [rate, setRate]=useState([]);
+  
+  useEffect(() => {
+    const newRate = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < rating) {
+        newRate.push(<img className={styles.starsRating} src={fullStar} alt="Etoile pleine" key={i} />);
+      } else {
+        newRate.push(<img className={styles.starsRating} src={star} alt="Etoile vide" key={i} />);
+      }
     }
-  }
-  return <div>{rate}</div>
+    setRate(newRate);
+  }, [rating]);
+  
+  return <div>{rate}</div>;
 }
 
 export default Rating
+
+
